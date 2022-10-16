367dc6cc216cbcf81e2911df23a51727a2f21b45
Samuel Rembisz <sjrembisz07@gmail.com>
2022-10-15 23:12:13 -0400

    Added entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -f /app/Rocket.toml ]; then
+    echo "[release]
+address = \"0.0.0.0\"
+port = 9999
+secret_key = \"$(LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c 88)\"
+[default.limits]
+forms = \"64 kB\"
+json = \"1 MiB\"
+msgpack = \"2 MiB\"
+\"file/jpg\" = \"5 MiB\"" > /app/Rocket.toml
+fi
+
+# Run service
+/app/hogbisz_cdn
